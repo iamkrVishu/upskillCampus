a7cbb5aa5394ff25cea0fd816dccc74892bb691b
Vishvajeet Kumar <kumarvishvajeetsingh@gmail.com>
2024-10-26 01:24:29 +0530

    first commit

---
+++ Frontend/src/context/AuthProvider.jsx
@@ -0,0 +1,27 @@
+import React, { createContext, useContext, useEffect, useState } from "react";
+
+export const AuthContext = createContext();
+
+export default function AuthProvider({ children }) {
+  const initialAuthUser = localStorage.getItem("Users");
+  const [authUser, setAuthUser] = useState(
+    initialAuthUser ? JSON.parse(initialAuthUser) : undefined
+  );
+
+  useEffect(() => {
+    // Update localStorage whenever authUser changes
+    if (authUser) {
+      localStorage.setItem("Users", JSON.stringify(authUser));
+    } else {
+      localStorage.removeItem("Users");
+    }
+  }, [authUser]);
+
+  return (
+    <AuthContext.Provider value={[authUser, setAuthUser]}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
+
+export const useAuth = () => useContext(AuthContext);
